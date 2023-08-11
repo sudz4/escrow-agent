0a1bfbc29e48237736fd4b1c637f73dc171a55e3
sudz4 <mwsuther@syr.edu>
2023-08-10 17:37:13 -0700

    fc

---
+++ tic-tac-toe_game/ttt_game_server.py
@@ -0,0 +1,51 @@
+def print_board(board):
+    for row in board:
+        print(' | '.join(row))
+        print('-' * 101)
+
+def is_winner(board, player):
+    # Check rows, columns, and diagonals
+    for row in board:
+        if all(s == player for s in row):
+            return True
+    for col in range(3):
+        if all(board[row][col] == player for row in range(3)):
+            return True
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+    return False
+
+def is_draw(board):
+    return all(all(cell != ' ' for cell in row) for row in board)
+
+def main():
+    board = [[' ' for _ in range(3)] for _ in range(3)]
+    players = ['X', 'O']
+    turn = 0
+
+    while True:
+        print_board(board)
+        print(f"Player {players[turn % 2]}'s turn:")
+        row = int(input("Enter row (0-2): "))
+        col = int(input("Enter column (0-2): "))
+
+        if board[row][col] != ' ':
+            print("Cell is already occupied. Try again.")
+            continue
+
+        board[row][col] = players[turn % 2]
+
+        if is_winner(board, players[turn % 2]):
+            print_board(board)
+            print(f"Player {players[turn % 2]} wins!")
+            break
+
+        if is_draw(board):
+            print_board(board)
+            print("It's a draw!")
+            break
+
+        turn += 1
+
+if __name__ == "__main__":
+    main()
